f1a16029063700669414baa3c7447a751468754d
FipFippington <116121197+FipFippington@users.noreply.github.com>
2024-03-25 13:53:24 -0700

    saMBNJK SDF
    GSERGTD

---
+++ GraphApiValenzuela/Assets/Graphs/Graph.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Graph 
+{
+    List<Edge> edges = new List<Edge>();
+    List<Node> nodes = new List<Node>();
+    List<Node> pathList = new List<Node>();
+
+    public Graph() { }
+
+    public void AddNode(GameObject id)
+    {
+        Node node = new Node(id);
+        nodes.Add(node);
+    }
+
+    public void AddEdge(GameObject fromNode, GameObject toNode)
+    {
+        Node from = FindNode(fromNode);
+        Node to = FindNode(toNode);
+
+        if(from != null && to != null)
+        {
+            Edge e = new Edge(from, to);
+            edges.Add(e);
+
+            from.edgeList.Add(e);
+        }
+    }
+
+    Node FindNode(GameObject id)
+    {
+        foreach (Node n in nodes)
+        {
+            if(n.getId() == id)
+                return n;
+        }
+        return null;
+    }
+
+    public bool AStar(GameObject startId, GameObject endId)
+    {
+        Node start = FindNode(startId);
+        Node end = FindNode(endId);
+
+        if (start == null || end == null)
+        { 
+            return false;
+        }
+
+        List<Node> open = new List<Node>();
+        List <Node> close = new List<Node>();
+        float tentative_g_score = 0;
+        bool tentative_is_better;
+
+        start.g = 0;
+        start.h = distance(start, end);
+        start.f = start.h;
+
+        open.Add(start);
+        while (open.Count > 0)
+        {
+            int i = lowestF(open);
+            Node thisNode = open[i];
+            if (thisNode.getId() == endId)
+            {
+                return true;
+            }
+
+            open.RemoveAt(i);
+            closed.Add(thisNode);
+
+        }
+    }
+
+    float distance(Node a, Node b)
+    {
+        return (Vector3.SqrMagnitude(a.getId().transform.position - b.getId().transform.position));
+    }
+
+    int lowestF(List<Node>l)
+    {
+        float lowestf = 0;
+        int count = 0;
+        int iteratorCount = 0;
+
+        lowestf = l[0].f;
+
+        for(int i = 1; i < l.Count; i++)
+        {
+            if (l[i].f <= lowestf)
+            {
+                lowestf = l[i].f;
+                iteratorCount = count;
+            }
+            count++;
+        }
+        return iteratorCount;
+    }
+}
